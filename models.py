65c64cb45a25778ca2aefd2d59fa3b9fe520f6ca
John Gaitho <jgaitho016@gmail.com>
2025-02-24 16:13:25 +0300

    user table model

--- models.py
+++ models.py
@@ -1,1 +1,12 @@
-sqlite
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50))
+    email = db.Column(db.String(100))
+    password_hash = db.Column(db.String(255))
