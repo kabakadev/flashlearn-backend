e36dc4b15d6706cb7c46919909587563d631f311
John Gaitho <jgaitho016@gmail.com>
2025-02-24 17:27:36 +0300

    app imports

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+db.init_app(app) 
+migrate = Migrate(app, db)
+jwt = JWTManager(app)
+CORS(app)
 
 
 if __name__ == "__main__":
