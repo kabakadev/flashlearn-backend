59fa92dc8a9e33dfc25f811b2f6f42bde068f272
John Gaitho <jgaitho016@gmail.com>
2025-02-24 16:58:55 +0300

    register and login

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,12 +1,32 @@
-from flask import request, jsonify
+from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token
-from models import User
+from models import db, User
+
+auth_bp = Blueprint("auth", __name__)
 
 
-
-@route("/register", methods=["POST"])
+@auth_bp.route("/register", methods=["POST"])
 def register():
     data = request.json
     user = User.query.filter((User.username == data["username"]) | (User.email == data["email"])).first()
     
+    if user:
+        return jsonify({"message": "User already exists"}), 400
     
+    new_user = User(username=data["username"], email=data["email"])
+    new_user.set_password(data["password"])
+    db.session.add(new_user)
+    db.session.commit()
+    
+    return jsonify({"message": "User registered"}), 201
+
+@auth_bp.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    user = User.query.filter_by(email=data["email"]).first()
+
+    if user and user.check_password(data["password"]):
+        access_token = create_access_token(identity=user.id)
+        return jsonify({"access_token": access_token}), 200
+
+    return jsonify({"message": "Invalid credentials"}), 401
