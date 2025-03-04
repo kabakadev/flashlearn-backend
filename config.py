f9e3696dddbbd683901806e6ec2bfe59c0629120
John Gaitho <jgaitho016@gmail.com>
2025-03-04 09:13:07 +0300

    added cors

--- config.py
+++ config.py
@@ -3,7 +3,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
+app = Flask(__name__)
 class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///flashlearn.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -13,3 +15,5 @@ class Config:
 db = SQLAlchemy()
 api = Api()
 jwt = JWTManager()
+
+CORS(app, supports_credentials=True, resources={r"/*": {"origins": "*"}})
