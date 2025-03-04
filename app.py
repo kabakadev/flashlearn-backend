4a567e33112e94423e2eb96f68dc498a8dfb5018
John Gaitho <jgaitho016@gmail.com>
2025-03-04 10:40:11 +0300

    user authentication

--- app.py
+++ app.py
@@ -2,16 +2,20 @@ from flask import Flask
 from flask_restful import Api
 from flask_migrate import Migrate
 from config import db, jwt, Config
-from routes.auth_routes import Signup, Login
+from routes.auth_routes import Signup, Login, UserResource
 from routes.deck_routes import DeckListResource, DeckResource
 from routes.flashcard_routes import FlashcardListResource, FlashcardResource
 from routes.progress_routes import ProgressResource
 from routes.stats_routes import UserStatsResource
 from routes.dashboard_routes import Dashboard
+from flask_cors import CORS
 
 # Initialize Flask App
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(Config)
+
+
 
 # Initialize Extensions
 db.init_app(app)
@@ -25,6 +29,7 @@ from models import User, Deck, Flashcard
 # Register API Endpoints
 api.add_resource(Signup, "/signup")
 api.add_resource(Login, "/login")
+api.add_resource(UserResource, "/user")
 api.add_resource(DeckListResource, "/decks")
 api.add_resource(DeckResource, "/decks/<int:deck_id>")
 api.add_resource(FlashcardListResource, "/decks/<int:deck_id>/flashcards")
